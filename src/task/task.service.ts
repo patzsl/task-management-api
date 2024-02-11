19c5679fddb036b30e37bc200d29f5f56e53702b
Gabriel Maciel Teixeira <gabriel.teixeira@7comm.com.br>
2024-02-11 17:18:54 -0300

    feat: create task service and controller

---
+++ src/task/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+    private tasks: TaskDto[] = [];
+
+    create(task: TaskDto) {
+        console.clear();
+        this.tasks.push(task);
+        console.log(this.tasks);
+    }
+}
